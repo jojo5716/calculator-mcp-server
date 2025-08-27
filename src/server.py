788bc35d614252289f9e58e27b42dc9d26b73935
Jonathan Rodríguez Alejos <jrodriguez.5716@gmail.com>
2025-08-27 13:31:59 +0200

    adding initial files

---
+++ src/server.py
@@ -0,0 +1,29 @@
+from starlette.applications import Starlette
+from starlette.routing import Mount, Host
+from mcp.server.fastmcp import FastMCP
+
+server = FastMCP("SSE")
+
+
+
+@server.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers."""
+    return a + b
+
+@server.tool()
+def substract(a: int, b: int) -> int:
+    """Subtract two numbers."""
+    return a - b
+
+@server.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers."""
+    return a * b
+
+@server.tool()
+def divide(a: int, b: int) -> float:
+    """Divide two numbers."""
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
